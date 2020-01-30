cfdd24bc90ceb6f1b79e1a2c3b4e4092ab928f6c
rlaguswhd19 <rlaguswhd19@naver.com>
2020-01-30 14:27:37 +0900

    jar 파일 만들기

---
+++ jar/src/main/java/hj/springboot/study/JarApplication.java
@@ -0,0 +1,21 @@
+package hj.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JarApplication {
+
+	@GetMapping("/")
+	public String Hello() {
+		return "HELLO";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JarApplication.class, args);
+	}
+
+}
