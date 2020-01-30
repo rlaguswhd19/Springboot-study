d49e9e3f1d9cb837898fe1675f1729aa01884efc
rlaguswhd19 <rlaguswhd19@naver.com>
2020-01-30 15:23:48 +0900

    banner change

--- jar/src/main/java/hj/springboot/study/JarApplication.java
+++ jar/src/main/java/hj/springboot/study/JarApplication.java
@@ -1,9 +1,15 @@
 package hj.springboot.study;
 
+import java.io.PrintStream;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @SpringBootApplication
 @RestController
@@ -15,7 +21,21 @@ public class JarApplication {
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(JarApplication.class, args);
+		SpringApplication app = new SpringApplication(JarApplication.class);
+		app.setBanner(new Banner() {
+
+			@Override
+			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+				// TODO Auto-generated method stub
+				out.println("======================================");
+				out.println("KIM HYEON JONG SPINRG BOOT");
+				out.println("======================================");
+			}
+			
+		});
+//		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+		
 	}
 
 }
