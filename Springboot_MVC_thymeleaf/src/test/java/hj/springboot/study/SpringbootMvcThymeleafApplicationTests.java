e36136d41387fb05fbb32c17c7802de6c7d80e6d
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-04 11:39:06 +0900

    thymeleaf

---
+++ Springboot_MVC_thymeleaf/src/test/java/hj/springboot/study/SpringbootMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.springboot.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
