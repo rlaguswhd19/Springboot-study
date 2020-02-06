43c1f28723cb88a60999118e5abac7f300161214
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-06 18:02:35 +0900

    spring actuator

---
+++ Springboot_Actuator/src/main/java/hj/springboot/study/HomeController.java
@@ -0,0 +1,13 @@
+package hj.springboot.study;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
